4884c6d0115bc3e4b95c5e4721681347ba256fac
Abdurahmon <144538296+AbdurahmonML@users.noreply.github.com>
2024-11-02 16:58:52 +0300

    Create generate_images.py

---
+++ Autoencoder/generate_images.py
@@ -0,0 +1,25 @@
+def generate_images(model, num_samples, mean, std):
+    random_vectors = torch.normal(mean=mean_embedding.expand(num_samples, -1, -1, -1), 
+                               std=std_embedding.expand(num_samples, -1, -1, -1))*1.2
+    
+    with torch.no_grad():
+        decoded_images = model.decoder(random_vectors.cuda())  
+
+
+
+    num_images = decoded_images.shape[0]
+
+    plt.figure(figsize=(num_images * 2, 2))  
+
+    for i in range(num_images):
+        image = decoded_images[i].permute(1, 2, 0).cpu().numpy() 
+        
+        plt.subplot(1, num_images, i + 1)  
+        plt.imshow(image)
+        plt.axis('off')  
+
+    plt.tight_layout()  
+    plt.show()
+
+    return num_images
+

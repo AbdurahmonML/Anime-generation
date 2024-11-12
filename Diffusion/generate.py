443093cc1fd3a79e30cae8981aa0e73977b42ce0
Abdurahmon <144538296+AbdurahmonML@users.noreply.github.com>
2024-11-13 01:58:09 +0300

    Update generate.py

--- Diffusion/generate.py
+++ Diffusion/generate.py
@@ -1,3 +1,10 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.cuda import amp
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
 def generate(model, sd, num_images=8, timesteps=1000, nrow=16, format='png', device="cpu"):
     generate_video = False
 
